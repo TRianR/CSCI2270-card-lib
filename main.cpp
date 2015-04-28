64f8b1a466870ec0f4207c95f640c75ff89feb52
dtrahan41 <dtrahan41@googlemail.com>
2015-04-28 17:38:10 -0600

    Updated Main
    Organized and created user functionality in main.

--- main.cpp
+++ main.cpp
@@ -6,28 +6,69 @@ using namespace std;
 int main()
 {
     deck newDeck;
+    while(true){
+        cout<<"|======Testing Program======|"<<endl;
+        cout<<"1. Shuffle Deck"<<endl;
+        cout<<"2. Draw Card"<<endl;
+        cout<<"3. Replace Card"<<endl;
+        cout<<"4. Reset Deck"<<endl;
+        cout<<"5. Show Deck"<<endl;
+        cout<<"6. Change Suit"<<endl;
+        cout<<"7. Custom Deck"<<endl;
+        cout<<"8. Find Remaining"<<endl;
+        cout<<"9. Quit"<<endl;
 
-    card* newCard1 = newDeck.drawCard();
-    card* newCard2 = newDeck.drawCard();
-    card* newCard3 = newDeck.drawCard();
+        int userInput;
+        cin >> userInput;
 
-    cout<<newCard1->suite<<", "<<newCard1->value<<endl;
-    cout<<newCard2->suite<<", "<<newCard2->value<<endl;
-    cout<<newCard3->suite<<", "<<newCard3->value<<endl;
+        if(userInput == 1){
+            newDeck.shuffleDeck();
+        }
+        if(userInput == 2){
+            card* newCard1 = newDeck.drawCard();
+            cout<<newCard1->suit<<", "<<newCard1->value<<endl;
+        }
+        if(userInput == 3){
+            newDeck.replaceCard();
+        }
+        if(userInput == 4){
+            newDeck.resetDeck();
+        }
+        if(userInput == 5){
+            newDeck.showDeck();
+        }
+        if(userInput == 6){
+            string userInput2;
+            string userInput3;
 
-    newDeck.shuffleDeck();
+            cout<<"Old suit?"<<endl;
+            cin >> userInput2;
+            cout<<"New suit?"<<endl;
+            cin >>userInput3;
 
-    cout<<endl;
+            newDeck.changeSuit(userInput2, userInput3);
+        }
+        if(userInput == 7){
+        	cout<<"Nothing here yet!"<<endl;
+        }
+        if(userInput == 8){
 
-    newDeck.changeSuite("Hearts", "NathansStruggle");
+            string userInput4;
+            string userInput5;
 
-    newDeck.showDeck();
+            cout<<"Number"<<endl;
+            cin >> userInput4;
+            cout<<"Suit"<<endl;
+            cin >> userInput5;
 
-    cout<<endl;
-
-    newDeck.resetDeck();
-
-    newDeck.showDeck();
-	
-	newDeck.~deck();
+            card *testCard = new card;
+            testCard->suit = "Hearts";
+            testCard->value = 3;
+            newDeck.findRemaining(testCard);
+            delete testCard;
+        }
+        if(userInput == 9){
+            return 0;
+        }
+    }
 }
