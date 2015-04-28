e9f937d32f634e52ea941a6ff871c1455b02494e
Daniel Trahan <daniel.trahan@colorado,edu>
2015-04-28 04:27:47 -0600

    Added deck.h to final project

---
+++ deck.h
@@ -0,0 +1,37 @@
+#ifndef DECK_H
+#define DECK_H
+
+#include <iostream>
+#include <queue>
+
+struct card
+{
+    int value;
+    std::string suite;
+};
+
+class deck
+{
+    public:
+        deck();
+        virtual ~deck(); //Daniel
+        card* drawCard(); //Daniel
+        void shuffleDeck(); //Daniel
+        void replaceCard(); //Joaquin
+        void showDeck(); //Daniel
+        void resetDeck(); //Daniel
+        void irregularDeck(card arrayOfNewCards[]); //Joaquin
+        void changeSuite(std::string oldSuite, std::string newSuite); //Daniel
+        //add two more functions
+    protected:
+    private:
+        void constructDeck();
+
+        int deckSize = 52;
+        std::string suites[4] = {"Hearts", "Clubs", "Diamonds", "Spades"};
+        std::vector<card*> cardBuffer;
+        std::queue<card*> cardOrder;
+        std::queue<card*> removedCards;
+};
+
+#endif // DECK_H
